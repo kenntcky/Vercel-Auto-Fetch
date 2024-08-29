37fd9b8f94157fc8d7fd7ca90f5101c0afb80724
kenntcky <hologrampulsion@gmail.com>
2024-08-29 13:16:34 +0700

    Initialize auto fetch data

---
+++ api/index.js
@@ -0,0 +1,55 @@
+const axios = require('axios');
+const admin = require('firebase-admin');
+const BMKG_API_URL = 'https://data.bmkg.go.id/DataMKG/TEWS/autogempa.json';
+const serviceAccount = require("belajar-firebase-777-firebase-adminsdk-r9s8a-dd714e8a5a.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://belajar-firebase-777-default-rtdb.asia-southeast1.firebasedatabase.app"
+});
+
+const db = admin.database();
+
+async function fetchAndStoreData() {
+  try {
+    const response = await axios.get(BMKG_API_URL);
+    if (response.status === 200) {
+      const latestData = response.data;
+
+      const dateTime = latestData['Infogempa']['gempa']['DateTime'];
+      const shakemapFile = latestData['Infogempa']['gempa']['Shakemap'];
+      const shakemapUrl = `https://data.bmkg.go.id/DataMKG/TEWS/${shakemapFile}`;
+      latestData['Infogempa']['gempa']['shakemapUrl'] = shakemapUrl;
+
+      const refDate = db.ref(dateTime);
+      const ref = db.ref();
+      const snapshot = await ref.once('value');
+
+      if (snapshot.exists()) {
+        ref.orderByKey().limitToLast(1).on('value', async (snapshot) => {
+          const data = snapshot.val();
+          const latestKey = Object.keys(data)[0];
+
+          if (latestKey !== dateTime) {
+            await refDate.set(latestData);
+            console.log('New earthquake data has been saved in Firebase Realtime Database!');
+          } else {
+            console.log('No new earthquake data.');
+          }
+        });
+      } else {
+        await refDate.set(latestData);
+        console.log('New earthquake data has been saved in Firebase Realtime Database!');
+      }
+    } else {
+      console.log('Failed to load data');
+    }
+  } catch (e) {
+    console.error('Error fetching data:', e);
+  }
+}
+
+module.exports = async (req, res) => {
+  await fetchAndStoreData();
+  res.status(200).send('Data fetched and stored');
+};
